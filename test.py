4fe758deed610127e53ddc9b14f2acc7b15768f2
Stefan Kögl <stefan@skoegl.net>
2013-10-24 20:13:23 +0200

    refactor tests

--- test.py
+++ test.py
@@ -6,27 +6,28 @@ import podcastparser
 
 class ParseFeedTest(unittest.TestCase):
 
-    def test_feed(self):
+    @classmethod
+    def setUpClass(self):
         """ Parse example podcast and verify its results """
         URL = 'http://example.com/feed/'
         FILE = 'tests/example-feed.xml'
-        result = podcastparser.parse(URL, open(FILE))
-        self.assertTrue(bool(result))
+        self.result = podcastparser.parse(URL, open(FILE))
 
-        self.assertEqual('Podcast Title', result.get('title'))
-        self.assertEqual('Some description', result.get('description'))
+    def test_podcast(self):
+        self.assertEqual('Podcast Title', self.result.get('title'))
+        self.assertEqual('Some description', self.result.get('description'))
         self.assertEqual('https://flattr.com/submit/auto?user_id=123&url='
                          'http%3A%2F%2Fexample.com%2F&language=de_DE&category'
                          '=audio&title=Podcast&description=A+Podcast&tags='
-                         'podcast', result.get('payment_url'))
+                         'podcast', self.result.get('payment_url'))
         self.assertEqual('http://example.com/image.png',
-                         result.get('cover_url'))
-        self.assertEqual('http://example.com', result.get('link'))
+                         self.result.get('cover_url'))
+        self.assertEqual('http://example.com', self.result.get('link'))
 
-        self.assertEqual(4, len(result.get('episodes', [])))
+        self.assertEqual(4, len(self.result.get('episodes', [])))
 
-        # Episode 1
-        episode = result.get('episodes')[0]
+    def test_episode1(self):
+        episode = self.result.get('episodes')[0]
         self.assertEqual(8160, episode.get('total_time'))
         self.assertEqual('https://flattr.com/submit/auto?user_id=123&url='
                          'http%3A%2F%2Fexample.com%2Fepisode-1&language=de_DE'
@@ -47,10 +48,10 @@ class ParseFeedTest(unittest.TestCase):
         self.assertEqual('audio/ogg', enclosure.get('mime_type'))
         self.assertEqual(50914623, enclosure.get('file_size'))
 
-
-        # Episode 2
-        episode = result.get('episodes')[1]
-        self.assertEqual('http://example.com/podcast/episode/2/', episode.get('guid'))
+    def test_episode2(self):
+        episode = self.result.get('episodes')[1]
+        self.assertEqual('http://example.com/podcast/episode/2/',
+                         episode.get('guid'))
 
         # the GUID is used as the link
         self.assertEqual(episode.get('link'), episode.get('guid'))
@@ -58,34 +59,37 @@ class ParseFeedTest(unittest.TestCase):
         # the basename of the enclosure is used as a title
         self.assertEqual('episode-2', episode.get('title'))
 
-
-        # Episode 3
-        episode = result.get('episodes')[2]
+    def test_episode3(self):
+        episode = self.result.get('episodes')[2]
         self.assertEqual('http://example.com/episode/3/', episode.get('link'))
 
         # the link is used as the GUID
         self.assertEqual(episode.get('link'), episode.get('guid'))
 
-
-        # Episode 4
-        episode = result.get('episodes')[3]
+    def test_episode4(self):
+        episode = self.result.get('episodes')[3]
         # the enclosure URL is used as the GUID
         self.assertEqual('http://example.com/episode/episode-4.ogg',
                          episode.get('guid'))
 
 
-    def test_feed(self):
+class MaxEpisodesTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
         """ Parse example podcast with an episode limit """
         URL = 'http://example.com/feed/'
         FILE = 'tests/example-feed.xml'
-        result = podcastparser.parse(URL, open(FILE), max_episodes=2)
+        self.result = podcastparser.parse(URL, open(FILE), max_episodes=2)
 
-        self.assertEqual(2, len(result.get('episodes', [])))
+    def test_max_episodes(self):
+        self.assertEqual(2, len(self.result.get('episodes', [])))
 
 
 suite = unittest.TestSuite()
 suite.addTest(doctest.DocTestSuite(podcastparser))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ParseFeedTest))
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(MaxEpisodesTest))
 
 runner = unittest.TextTestRunner(verbosity=1)
 result = runner.run(suite)
