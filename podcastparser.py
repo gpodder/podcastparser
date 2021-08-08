119c87dbdd03a61d6e8b1d7b3dfe8b79ee484ab0
Damir Chanyshev <damikoska1@gmail.com>
2021-08-08 22:49:50 +0500

    add support for 'itunes:new-feed-url'

--- podcastparser.py
+++ podcastparser.py
@@ -684,6 +684,7 @@ MAPPING = {
     'rss/channel/itunes:author': PodcastAttr('itunes_author', squash_whitespace),
     'rss/channel/itunes:owner': ItunesOwnerItem('itunes_owner', squash_whitespace),
     'rss/channel/itunes:explicit': PodcastAttrExplicit('explicit', squash_whitespace),
+    'rss/channel/itunes:new-feed-url': PodcastAttr('new_feed_url', squash_whitespace),
 
     'rss/channel/itunes:owner/itunes:email': ItunesOwnerAttr('email', squash_whitespace),
     'rss/channel/itunes:owner/itunes:name': ItunesOwnerAttr('name', squash_whitespace),
